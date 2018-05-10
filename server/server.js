fd0f36c343e636a3d02582bdcfea25190a13c6e5
Olaf Vaher <olafvaher@Olafs-MacBook-Pro.local>
2018-05-10 17:52:59 +0200

    add setInterval

--- server/server.js
+++ server/server.js
@@ -1,11 +1,18 @@
 const path = require('path');
 const express = require('express');
+const http = require('http');
 
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
+
+// ping app in every 10 minutes (600000) to prevent Heroku sleeping
+setInterval(() => {
+  http.get('https://ov86-ntc-proxy-api.herokuapp.com/');
+  http.get('https://ov86-ntc-react-blog.herokuapp.com/');
+}, 600000);
 
 // match all unmatched routes as routing is handled by react-router
 app.get('*', (req, res) => {
